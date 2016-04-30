a1ae1498746ac890d060af0fe0fdcfaead1aeca3
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-04-30 06:46:05 -0400

    Add validation to Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ActiveRecord::Base
   PAYMENT_TYPES = ['Check', 'Credit Card', 'Purchase Order']
+
+  validates :name, :address, :email, presence: ture
+  validates :pay_type, inclusion: PAYMENT_TYPES
 end
