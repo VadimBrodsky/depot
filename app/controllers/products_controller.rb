9ff73155ebaf064a7e2df85659624d751e1e3edc
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-05-02 06:22:33 -0400

    Add who_bought action that will generate an atom feed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -61,6 +61,17 @@ class ProductsController < ApplicationController
     end
   end
 
+  def who_bought
+    @product = Product.find(params[id])
+    @latest_order = @product.orders.order(:update_at).last
+    # if order did not change, respond with empty
+    if stale?(@latest_order)
+      respond_to do |format|
+        format.atom
+      end
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
