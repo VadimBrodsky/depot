9b7cec197f13de5c5c57345d7c492c7415b255c3
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-05-03 06:46:22 -0400

    Fix typo in the default from email address

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -1,5 +1,5 @@
 class OrderNotifier < ApplicationMailer
-  default from: 'Depot Store <depot@example.com'
+  default from: 'Depot Store <depot@example.com>'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
