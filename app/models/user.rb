614ffc4023700d46f6265e8a17b75c6e8f5b77d2
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-05-06 06:17:45 -0400

    Add restriction that will stop the last admin from being deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+  # Will revert the transaction if an error is raised
+  after_destroy :ensure_an_admin_remains
+
+  private
+
+  # This exception will be signaled back to the controller
+  # To avoid this use the ActiveRecord::Base.transaction exception
+  def ensure_an_admin_remains
+    if User.count.zero?
+      raise "Can't delete last user"
+    end
+  end
 end
